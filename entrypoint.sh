fd35a7cd92e5f92bc217c396592366a87c89c915
xia <50513568+artxia@users.noreply.github.com>
2022-01-24 23:59:23 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# fix WARNING overcommit_memory is set to 0! Background save may fail under low memory condition.
+
+sysctl vm.overcommit_memory=1
+
+# start redis server
+
 redis-server /etc/redis/redis.conf 2>&1 &
 python main.py
